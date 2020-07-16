b49382b8577d6f8bb1300911ffe108686a11b47b
mhws2 <mhws2@MHWS2>
2020-07-16 23:15:46 +0800

    23点15分

--- 07-11.c
+++ 07-11.c
@@ -17,9 +17,38 @@ void m11_2_2(){
     return;
 }
 
+void m11_4_1(){
+    int input = 1;
+    printf("11-4-1 输出一个菜单，如下所示：\n");
+    printf("01 开始游戏\n");
+    printf("02 游戏设置\n");
+    printf("03 退出\n");
+    while (input!=3)
+    {
+        scanf("%d",&input);
+        switch (input)
+        {
+        case 1:
+            printf("01 开始游戏\n");
+            break;
+        case 2:
+            printf("02 游戏设置\n");
+            break;
+        case 3:
+            printf("03 退出\n");
+            break;
+        default:
+            break;
+        }
+    }
+    return;
+}
+
 int main()
 {
     m11_2_2();
+    m11_4_1();
+    
     printf("\n");
     system("pause");
     return 0;
