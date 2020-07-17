8a51ec5b492da02b52da98180ee0765a20870df1
mhws2 <mhws2@MHWS2>
2020-07-17 14:43:53 +0800

    增加15-5-1阶乘计算

--- 07-15.c
+++ 07-15.c
@@ -20,12 +20,31 @@ void m15_4_7(){
     printf("\n结果\t%lld\n",sum);
 }
 
+void m15_5_1(){
+    int n=0,i=0,sum=0,yu=0;
+    size_t m=1;
+    printf("15-5-1 从键盘获取一个数字n，能够计算出任意n!尾部的0的个数。\n");
+    printf("(阶乘 例如：12！为479001600.得出的结果中尾部0的个数为2。)\n");
+    scanf("%d",&n);
+    while (++i<=n)
+        m*=i;
+    do
+    {
+        yu=m%10;
+        if (yu==0) sum++;
+        printf("yu=%d\t%lld\n",yu,m);
+        m/=10;
+    } while (yu==0);
+    printf("\n结果\t%lld\n",sum);
+}
+
 int main()
 {
     char a='A';
     
     printf("%c\n\n",a<'a'?a+32:a);
     m15_4_7();
+    m15_5_1();
     system("pause");
     return 0;
 }
