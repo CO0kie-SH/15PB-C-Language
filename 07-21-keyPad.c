35fb683816a8108519e64808076e48e71e7cef84
mhws2 <mhws2@MHWS2>
2020-07-21 22:17:51 +0800

    新增密码本函数

---
+++ 07-21-keyPad.c
@@ -0,0 +1,17 @@
+/*  IDE     :   VSCode
+    Coding  :   GB2312
+    TimeDate:   20200721-2200
+    By      :   Hongwei.Mao
+    Ver     :   V0.1*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    char a='A';
+    
+    printf("\n");
+    system("pause");
+    return 0;
+}
